7a6248b33c98da725175af28cdb0c5d62b7be9c0
Candice Peters <candicelizabethpeters@gmail.com>
2019-08-28 20:56:54 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,10 +1,21 @@
 class Author 
   attr_accessor :name
+  @@post_count = 0
   def initialize(name)
     @name = name 
   end
   
   def add_posts(post)
     post.artist = self 
+    @@post_count += 1
   end
+  
+  def add_post_by_title 
+    post = Post.new
+  end
+  def post_count 
+    @@post_count
+  end
+  
+  def 
 end
