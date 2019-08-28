7f8d2d9b9dedb76b7f225a3394fcbced66f390b7
Candice Peters <candicelizabethpeters@gmail.com>
2019-08-28 21:01:18 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,23 +1,22 @@
 class Author 
   attr_accessor :name
-  @@post_count = 0
+  @@post_counter = 0
   def initialize(name)
     @name = name 
   end
   
   def add_post(title)
     title.author = self 
-    @@post_count += 1
+    @@post_counter += 1
   end
   
   def add_post_by_title(title)
     title = Post.new(title)
     add_post(title)
-     @@song_counter += 1
-    
+     @@post_counter += 1
   end
   def post_count 
-    @@post_count
+    @@post_counter
   end
   
   def 
